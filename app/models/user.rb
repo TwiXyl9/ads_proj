51a7bb06f68773fb1e9bb4484e84bd69e2d7cf34
Ulian <kolkin.2002@mail.ru>
2021-12-27 12:48:12 +0300

    Add reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :avatar
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -48,14 +48,7 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  def downcase_email
-    self.email = email.downcase
-  end
 
-  def create_activation_digest
-    self.activation_token  = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
 
   def activate
     update_attribute(:activated,    true)
@@ -65,5 +58,27 @@ class User < ApplicationRecord
   # Отправляет электронное письмо для активации.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Отправляет электронное письмо для сброса пароля.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
