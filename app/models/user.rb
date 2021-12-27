0169f32a110b9f0022587a49ed80408512aad697
Ulian <kolkin.2002@mail.ru>
2021-12-27 12:02:59 +0300

    Add email activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,9 +37,10 @@ class User < ApplicationRecord
   end
 
   # Возвращает true, если предоставленный токен совпадает с дайджестом.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Забывает пользователя
@@ -54,5 +55,15 @@ class User < ApplicationRecord
   def create_activation_digest
     self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
+  end
+
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Отправляет электронное письмо для активации.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 end
